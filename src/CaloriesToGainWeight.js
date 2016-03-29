cf0880a8eefa0e1c9a7647cc93bb0e8ef936a08a
malkio <coding.malkio@gmail.com>
2016-03-29 20:56:21 +0800

    Refactored internals, api still thesame

---
+++ src/CaloriesToGainWeight.js
@@ -0,0 +1,15 @@
+
+/*\
+|*|
+|*|
+|*|
+|*| 2.)
+|*| BodyMassIndex
+\*/
+var CaloriesToGainWeight = function(_plugin, options){
+	this.options = options;
+	this.element = $(_plugin.element);
+	this.res = {
+
+	}
+}
